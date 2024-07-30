74367bc1a81b34937ea95eec16890668d5669e3d
msaqibdev <msaqibdev@gmail.com>
2024-07-31 00:24:54 +0500

    feat: Update tintColorLight and tintColorDark in Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,8 +3,8 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0369a1';
-const tintColorDark = '#fff';
+const tintColorLight = '#16a34a';
+const tintColorDark = '#16a34a';
 
 export const Colors = {
   light: {
